d9d8a1a18fde2699106649b774cbf3c7279a674c
nafis bin <nafisbinnizam@gmail.com>
2025-09-05 03:03:42 +0600

    feat: shadow works for the button

---
+++ src/components/Button.jsx
@@ -0,0 +1,47 @@
+import clsx from "clsx";
+import Marker from "./Marker.jsx";
+
+const Button = ({ icon, children, href, containerClassName, onClick, markerFill }) => {
+    const Inner = () => (
+        <>
+            <span className="relative flex items-center min-h-[60px] px-4 g4
+             rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+                <span className="absolute -left-[1px]">
+                    <Marker fill={markerFill} />
+                </span>
+
+                {icon && (
+                    <img 
+                        src={icon} 
+                        alt="circle" 
+                        className="size-10 mr-5 object-contain z-10" 
+                    />
+                )}
+                <span>
+                    {children}
+                </span>
+            </span>
+        </>
+    )
+    
+    return href ? (
+        <a className={clsx('relative p-0.5 g5 rounded-2xl shadow-[0px_16px_24px_rgba(0,0,0,0.25),0px_-14px_48px_rgba(40,51,111,0.7)] group',
+         containerClassName)}
+         href={href}
+        //  style={{ boxShadow: 'var(--shadow-500)' }}
+        >
+            <Inner />
+        </a>
+    )
+    : (
+        <button className={clsx('relative p-0.5 g5 rounded-2xl shadow-[0px_16px_24px_rgba(0,0,0,0.25),0px_-14px_48px_rgba(40,51,111,0.7)] group',
+          containerClassName)}
+          onClick={onClick}
+        //   style={{ boxShadow: 'var(--shadow-500)' }}
+        >
+            <Inner />
+        </button>
+    )
+}
+
+export default Button
