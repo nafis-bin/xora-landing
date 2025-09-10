4437fb457e5f94a19ff3fada5ee8557c3829b34e
nafis bin <nafisbinnizam@gmail.com>
2025-09-10 19:44:03 +0600

    fix: now it looks similar

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,5 +1,6 @@
 import { Element } from "react-scroll";
-import { features } from '../constants';
+import { features, details } from "../constants/index.jsx";
+import Button from "../components/Button.jsx";
 
 const Features = () => {
     return (
@@ -7,14 +8,53 @@ const Features = () => {
             <Element name="features">
                 <div className="container">
                     <div className="relative flex md:flex-wrap flex-nowrap border-2
-                     border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after 
+                     border-s3 rounded-[40px] md:overflow-hidden max-md:flex-col feature-after 
                      md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
-                        {features.map(feature => (
-                            <div key={feature.id}>
-                                {feature.title}
+                        {features.map(({ id, icon, title, caption, text, button }) => (
+                            <div 
+                                key={id}
+                                className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50
+                                max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl
+                                max-md:flex-320"
+                            >
+                                <div className="w-full flex justify-start items-start">
+                                    <div className="-ml-3 mb-12 flex items-center justify-center
+                                    flex-col">
+                                        <div className="w-0.5 h-16 bg-s3" />
+                                        <img 
+                                            src={icon} 
+                                            className="size-28 object-contain"
+                                            alt={title}
+                                        />
+                                    </div>
+                                </div>
+
+                                <p className="caption mb-5 max-md:mb-6">{caption}</p>
+                                <h2 className="max-w-[400px] mb-7 h3 text-p4 max-md:mb-6
+                                max-md:h5">
+                                    {title}
+                                </h2>
+                                <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">
+                                    {text}
+                                </p>
+                                <Button icon={button.icon}>{button.title}</Button>
+
                             </div>
                         ))}
+
+                        <ul className="relative flex justify-around flex-grow px-[5%]
+                        border-2 border-s3 rounded-[40px] max-md:hidden">
+                            <div className="absolute bg-s3/20 top-[38%] left-0 right-0 w-full
+                            h-[1px] z-10" />
+                            {details.map(({ id, icon, title }) => (
+                                <li key={id} className="relative pt-16 px-4 pb-14">
+
+                                </li>
+                            ))}
+                        </ul>
+
                     </div>
+
                 </div>
             </Element>
         </section>
